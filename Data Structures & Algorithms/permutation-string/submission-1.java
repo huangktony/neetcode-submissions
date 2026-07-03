e3dc1e45d345f3dfbc55af8592dc6aa2bf5c4fea
huangktony <121047686+huangktony@users.noreply.github.com>
2026-07-02 23:10:03 -0500

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+
+        int[] s1Freq = new int[26];
+        int[] s2Freq = new int[26];
+ 
+        for(int i = 0; i < s1.length(); i++){
+            s1Freq[s1.charAt(i) - 'a']++;
+            s2Freq[s2.charAt(i) - 'a']++;
+        }
+
+        if(Arrays.equals(s2Freq, s1Freq)){
+            return true;
+        }
+
+
+        for(int right = s1.length(); right < s2.length(); right++){
+            s2Freq[s2.charAt(right) - 'a']++;
+            s2Freq[s2.charAt(right - s1.length()) - 'a']--;
+            if(Arrays.equals(s2Freq, s1Freq)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
